da243b3bc730d63ba92ffb05ac4965cdb131f8e5
judepierre4477 <judepierre4477@gmail.com>
2023-11-20 23:26:45 -0500

    Main site files

---
+++ site.js
@@ -0,0 +1,62 @@
+try{
+    const Button = document.querySelector(".buttonn");
+    Button.addEventListener('click', renderData);
+
+    const generateNumber = () =>{
+    return Math.ceil(Math.random() * 1017);
+}
+
+function renderData(){
+    let index = generateNumber();
+    fetch( `https://pokeapi.co/api/v2/pokemon/${index}`)
+        .then(response => response.json())
+        .then(pokemon=>{
+            generatePokemon(pokemon);
+        }
+
+        )
+   }
+
+    function generatePokemon(pokemon){
+        const pane = document.querySelector(".pane");
+        const renderData = document.querySelector(".render-img");
+        const renderText = document.querySelector(".render-text");
+        renderData.innerHTML=' ';
+        renderText.innerHTML= ' ';
+        console.log(pokemon);
+        let pokemonName = pokemon.forms[0].name;
+        let spriteLocationFront = pokemon.sprites.front_default;
+        let spriteLocationBack = pokemon.sprites.back_default;
+
+        let pokeNameText = document.createTextNode("Name: " + pokemonName);
+        let pokeImageFront = document.createElement('img');
+        let pokeImageBack = document.createElement('img');
+        pokeImageFront.src=spriteLocationFront;
+        pokeImageBack.src = spriteLocationBack;
+
+        renderText.appendChild(pokeNameText);
+        renderData.appendChild(pokeImageFront);
+        if(spriteLocationBack != null){
+            renderData.appendChild(pokeImageBack);
+
+        }
+    }
+    //Work In Progress
+    // function generatePokemonStats(pokemon, renderText){
+    //     let keys = ["abilities"];
+    //     keys.forEach((element) =>{
+    //         pokemon[element].forEach((elements)=>{
+    //           for(let key in elements){
+    //                 console.log(`${key}: ${elements[key]}`);
+                
+    //           }
+    //         })
+
+
+    //     })
+
+    // }
+}
+catch(error){
+    console.log(error);
+}
